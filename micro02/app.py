8abefa255bb78b11d34ee4a3f18ac59b3bc93811
Ubuntu <UsuarioCZ@MV-004.vfqlylei0qgebjdcompskldslg.cx.internal.cloudapp.net>
2025-12-11 21:43:55 +0000

    Commit inicial

---
+++ micro02/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hola():
+    return "Microservicio 2 Proyecto Final"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
